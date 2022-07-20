9fc6a73b8affe5f128d5c542125fd05437f2bb34
realbaotrung <realbaotrung@gmail.com>
2022-07-20 14:12:21 +0700

    Update pre-commmit

--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.{js,vue}': ['vue-cli-service lint'],
-  'src/**/*.{js,vue}': ['prettier --write src/**/*.{js,json,vue} --config .prettierrc.js --ignore-path .prettierignore'],
-};
